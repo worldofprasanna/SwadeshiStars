7a0ac253051fc84af608fd85da2791f2b102d01c
Prasanna <worldofprasanna@yahoo.com>
2013-05-20 21:14:04 +0530

    1. Added Spring dependency in pom.xml

---
+++ src/main/java/org/swadeshi/config/WebConfig.java
@@ -0,0 +1,12 @@
+package org.swadeshi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
